e470be7d54e31b28ea8b63af2a666b801ce368ed
Kamil Naja <kamil.naja@nexio.pl>
2026-01-21 20:50:38 +0100

    refactor: simplify route definitions by using a routes array

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,38 +2,41 @@ import NotesPage from "@/notes/NotesPage.vue";
 import ProjectsPage from "@/projects/ProjectsPage.vue";
 import StatsPage from "@/stats/StatsPage.vue";
 import Main from "@/views/Main.vue";
+import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router";
 import CheckListPage from "../checklists/CheckListPage.vue";
 
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "home",
+    component: Main,
+  },
+  {
+    path: "/projects",
+    name: "projects",
+    component: ProjectsPage,
+  },
+  {
+    path: "/checklists",
+    name: "checklists",
+    component: CheckListPage,
+  },
+  {
+    path: "/notes",
+    name: "notes",
+    component: NotesPage,
+  },
+  {
+    path: "/stats",
+    name: "stats",
+    component: StatsPage,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Main,
-    },
-    {
-      path: "/projects",
-      name: "projects",
-      component: ProjectsPage,
-    },
-    {
-      path: "/checklists",
-      name: "checklists",
-      component: CheckListPage,
-    },
-    {
-      path: "/notes",
-      name: "notes",
-      component: NotesPage,
-    },
-    {
-      path: "/stats",
-      name: "stats",
-      component: StatsPage,
-    },
-  ],
+  routes,
 });
 
 export default router;
